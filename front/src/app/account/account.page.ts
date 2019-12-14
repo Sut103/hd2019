b2ac56617f12cf37010aa5c2884fa711d753e91c
fumami <fumami0517@gmail.com>
2019-12-15 02:05:45 +0900

    my-seriesページに遷移する機能追加

--- front/src/app/account/account.page.ts
+++ front/src/app/account/account.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth/auth.service';
-import { HttpClient } from '@angular/common/http';
+import {NavController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-account',
@@ -8,24 +9,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
-  messages: {
-    series_id: string,
-  }[] = [];
   constructor(
       public auth: AuthService,
-      public http: HttpClient,
+      public navCtrl: NavController,
 ) { }
   signOut() {
     this.auth.authSignOut();
   }
-  ionViewDidEnter() {
-    this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series4')
-        .subscribe(data => {
-          // @ts-ignore
-          this.messages = data;
-          console.log(this.messages);
-        });
-  }
+    nextMySeries() {
+        this.navCtrl.navigateForward('/my-series-page');
+    }
   ngOnInit() {
   }
 
