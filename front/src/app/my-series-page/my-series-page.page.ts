b9b42e91b6941bbb65311ca3aa3166bcd78e714a
fumami <fumami0517@gmail.com>
2019-12-15 02:05:06 +0900

    仮データを入力

--- front/src/app/my-series-page/my-series-page.page.ts
+++ front/src/app/my-series-page/my-series-page.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NavController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-my-series-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-series-page.page.scss'],
 })
 export class MySeriesPagePage implements OnInit {
-
-  constructor() { }
+  messages: {
+    series_id: string,
+  }[] = [];
+  constructor(
+      public http: HttpClient,
+      public navCtrl: NavController,
+  ) { }
 
   ngOnInit() {
   }
-
+  ionViewDidEnter() {
+    this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series4')
+        .subscribe(data => {
+          // @ts-ignore
+          this.messages = data;
+          console.log(this.messages);
+        });
+  }
+  backAccount() {
+    this.navCtrl.navigateForward('/account');
+  }
 }
