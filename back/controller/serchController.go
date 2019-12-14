2e0865ece5c247edfe9e40f7e7ca4fd79d9b3ba4
EIMIKI <kou.103.kan@gmail.com>
2019-12-15 01:12:27 +0900

    serchapi追加

--- back/controller/serchController.go
+++ back/controller/serchController.go
@@ -1,8 +1,56 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
 
-func (ct *Controller) GetSerch(c *gin.Context) {
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
+)
 
+type ResponseGetSearch struct {
+	ID     string `json:"id"`
+	Latlng Latlng `json:"latlng"`
 }
 
+func (ct *Controller) GetSerch(c *gin.Context) {
+	length := 0.001
+	client := ct.Firestore.Client
+	ctx := ct.Firestore.Ctx
+	ress := []ResponseGetSearch{}
+	//request
+	req := Latlng{}
+	c.ShouldBindQuery(&req)
+	fmt.Println(req)
+	//lat search
+	iter := client.Collection("message").Where("latlng.lat", "<", req.Lat+length).Where("latlng.lat", ">", req.Lat-length).Documents(ctx)
+	for {
+		res := ResponseGetSearch{}
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		message := Message{}
+		err = doc.DataTo(&message)
+		if err != nil {
+			panic(err)
+		}
+
+		if !(message.Latlng.Lng < req.Lng+length && message.Latlng.Lng > req.Lng-length) {
+			continue
+		}
+
+		res.ID = doc.Ref.ID
+		res.Latlng.Lat = message.Latlng.Lat
+		res.Latlng.Lng = message.Latlng.Lng
+
+		ress = append(ress, res)
+		fmt.Println(message)
+	}
+
+	c.JSON(http.StatusOK, ress)
+}
