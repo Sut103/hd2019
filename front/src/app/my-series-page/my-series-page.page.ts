5de7462f78319b9a770ec863351e611f6fe50260
fumami <fumami0517@gmail.com>
2019-12-15 04:59:04 +0900

    仮データを表示した

--- front/src/app/my-series-page/my-series-page.page.ts
+++ front/src/app/my-series-page/my-series-page.page.ts
@@ -9,25 +9,36 @@ import {NavController} from '@ionic/angular';
   styleUrls: ['./my-series-page.page.scss'],
 })
 export class MySeriesPagePage implements OnInit {
-  messages: {
-    series_id: string,
+  series: {
+    id: string,
+      name: string
   }[] = [];
   constructor(
       public http: HttpClient,
       public navCtrl: NavController,
-  ) { }
+  ) {
+      this.series = [
+          { id: '0', name: 'series1' },
+          { id: '1', name: 'series2' },
+          { id: '2', name: '日記' },
+          { id: '3', name: 'ツイート' },
+      ];
+  }
 
   ngOnInit() {
   }
-  ionViewDidEnter() {
-    this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series4')
-        .subscribe(data => {
-          // @ts-ignore
-          this.messages = data;
-          console.log(this.messages);
-        });
-  }
+  // ionViewDidEnter() {
+  //   // this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series4')
+  //     this.http.get('https://floating-retreat-70851.herokuapp.com/series')
+  //
+  //         .subscribe(data => {
+  //         // @ts-ignore
+  //         this.series = data;
+  //         console.log(this.series);
+  //       });
+  // }
     moveMessage() {
-        this.navCtrl.navigateForward('/messages');
+        this.navCtrl.navigateForward('/messages', {
+        });
     }
 }
