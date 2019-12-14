8b277795b59264b9e70a234ba2e63ef0b2716f25
fumami <fumami0517@gmail.com>
2019-12-15 00:44:49 +0900

    getでmessage取得機能を追加

--- front/src/app/account/account.page.ts
+++ front/src/app/account/account.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-account',
@@ -7,10 +8,23 @@ import {AuthService} from '../auth/auth.service';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
-
-  constructor(public auth: AuthService) { }
+  messages: {
+    series_id: string,
+  }[] = [];
+  constructor(
+      public auth: AuthService,
+      public http: HttpClient,
+) { }
   signOut() {
     this.auth.authSignOut();
+  }
+  ionViewDidEnter() {
+    this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series4')
+        .subscribe(data => {
+          // @ts-ignore
+          this.messages = data;
+          console.log(this.messages);
+        });
   }
   ngOnInit() {
   }
