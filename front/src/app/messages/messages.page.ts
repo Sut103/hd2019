68ff5fffcee1a6c771024789762dac9e15e1e58c
fumami <fumami0517@gmail.com>
2019-12-15 04:58:51 +0900

    仮データを表示した

--- front/src/app/messages/messages.page.ts
+++ front/src/app/messages/messages.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
-
-  constructor() { }
+  messages: {
+    id: string,
+  }[] = [];
+  constructor(
+      public http: HttpClient,
+  ) {
+  }
 
   ngOnInit() {
   }
-
-}
+  ionViewDidEnter() {
+      this.http.get('https://floating-retreat-70851.herokuapp.com/messages?series_id=series1')
+            .subscribe(data => {
+            // @ts-ignore
+           this.messages = data;
+            console.log(this.messages);
+          });
+    }
+  }
