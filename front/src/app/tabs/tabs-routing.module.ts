c791fb92f602e923a70ab2d4d7c4444e74a431ee
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-12-14 13:13:02 +0900

    tabを変更

--- front/src/app/tabs/tabs-routing.module.ts
+++ front/src/app/tabs/tabs-routing.module.ts
@@ -8,45 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'map',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('../map/map.module').then(m => m.MapPageModule)
           }
         ]
       },
       {
-        path: 'tab2',
+        path: 'account',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../account/account.module').then(m => m.AccountPageModule)
           }
         ]
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/map',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/map',
     pathMatch: 'full'
   }
 ];
