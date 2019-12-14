f5857859c97f60c35fa4ee316e539c43ce59a088
fumami <fumami0517@gmail.com>
2019-12-14 19:10:04 +0900

    signOut機能を追加

--- front/src/app/account/account.page.ts
+++ front/src/app/account/account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-account',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
-
+  constructor(public auth: AuthService) { }
+  signOut() {
+    this.auth.authSignOut();
+  }
   ngOnInit() {
   }
 
